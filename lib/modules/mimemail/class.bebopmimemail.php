e52a2dcc5e97937c92b367d8f10df5a30d85f568
dmkfasi <dmkfasi@2458a54b-5a3f-0410-91e5-c925447d323a>
2008-08-12 18:34:25 +0000

    Исправлен баг с отправкой через встроенный smtp-транспорт. При заданном mail_server в конфигурации сайта, должен использоваться эмулятор smtp, иначе смысл в этой опции пропадает, php сам не умеет задавать адрес сервера (кроме версий win32).

--- lib/modules/mimemail/class.bebopmimemail.php
+++ lib/modules/mimemail/class.bebopmimemail.php
@@ -20,6 +20,7 @@ class BebopMimeMail implements iModuleConfig
 
     mcms::log('mail', t('to=%to, subject=%subject', array('%to' => join(',', $to), '%subject' => $subject)));
 
+    $transport = (null == BebopConfig::getInstance()->mail_server) ? 'mail' : 'smtp';
     $mail = new htmlMimeMail();
 
     $mail->setSMTPParams(mcms::config('mail_server'));
@@ -45,7 +46,7 @@ class BebopMimeMail implements iModuleConfig
         if (!empty($v))
           $mail->setHeader($k, $v);
 
-    return $mail->send($to);
+    return $mail->send($to, $transport);
   }
 
   public static function formGetModuleConfig()
